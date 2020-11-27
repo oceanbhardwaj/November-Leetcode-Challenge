583b5ab770c50766e0a651d4f55ef77c40c8bfd8
Ocean Bhardwaj <63634357+oceanbhardwaj@users.noreply.github.com>
2020-11-27 13:28:31 +0530

    Add files via upload

---
+++ LongestSubstringwithfreqk.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int longestSubstring(String s, int k) {
+        return helper(s.toCharArray(),0,s.length(),k);
+        
+    }
+    public int helper(char[]ch,int start,int end,int k)
+    {
+        if(k>end-start)
+            return 0;
+        int count[]=new int[26];
+        for(int i=start;i<end;i++)
+            count[ch[i]-'a']++;
+        for(int i=start;i<end;i++)
+        {
+            if(count[ch[i]-'a']<k)
+            {  int j=i+1;
+            
+             while(j<end && count[ch[j]-'a']<k)
+                 j++;
+             
+             
+             return Math.max(helper(ch,start,i,k),helper(ch,j,end,k));
+                
+                
+                
+                
+                
+             }
+            
+        }
+        return end-start;
+    }
+}
